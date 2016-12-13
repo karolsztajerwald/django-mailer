bceb2d40175d9bf3929ab8ffeb77048b4c0ac3bb
ksztajerwald <karol.sztajerwald@goldenline.pl>
2016-12-13 12:59:01 +0100

    Added new fields to admin

--- mailer/admin.py
+++ mailer/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 
 from mailer.models import Message, DontSendEntry, MessageLog
 
+import base64
+
 
 def show_to(message):
     return ", ".join(message.to_addresses)
@@ -22,7 +24,7 @@ class MessageAdminMixin(object):
 
 class MessageAdmin(MessageAdminMixin, admin.ModelAdmin):
 
-    list_display = ["id", show_to, "subject", "when_added", "priority"]
+    list_display = ["id", show_to, "subject", "when_added", "priority", "title", "to_address"]
     readonly_fields = ['plain_text_body']
     date_hierarchy = "when_added"
 
@@ -34,11 +36,18 @@ class DontSendEntryAdmin(admin.ModelAdmin):
 
 class MessageLogAdmin(MessageAdminMixin, admin.ModelAdmin):
 
-    list_display = ["id", show_to, "subject", "message_id", "when_attempted", "result"]
+    list_display = [
+        "id", show_to, "subject", "message_id", "when_attempted", "result", "show_message_data",
+        "title", "to_address",
+    ]
     list_filter = ["result"]
     date_hierarchy = "when_attempted"
     readonly_fields = ['plain_text_body', 'message_id']
     search_fields = ['message_id']
+
+    def show_message_data(self, message):
+        return base64.b64decode(message.message_data)
+    show_message_data.short_description = 'Message data'
 
 
 admin.site.register(Message, MessageAdmin)
